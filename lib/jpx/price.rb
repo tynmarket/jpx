90e3cb822a013db5f0f494d914dc43ccd70c9033
Takehiko Shinkura <tynmarket@gmail.com>
2020-03-01 14:38:43 +0900

    実際に取引が行われた日時に変換

--- lib/jpx/price.rb
+++ lib/jpx/price.rb
@@ -1,28 +1,42 @@
 require "csv"
 require "time"
+require "trading_day_jp"
 
 module Jpx
   module Price
     class << self
       def parse(path)
-        data = []
+        data = CSV.read(path)[1..-1].map do |row|
+          next if row.empty?
 
-        CSV.foreach(path, return_headers: false) do |row|
           # 取引日, _, 識別コード, セッション区分, 時刻, 始値, 高値, 安値, 終値, 出来高, VWAP, 約定回数, _, 限月（i = 13）
           datetime = Time.parse(row[0] + row[4])
 
-          if near_term?(datetime, row[13])
-            if row[3] == "003"
+          # 期近のみ取得
+          next unless near_term?(datetime, row[13])
 
-            end
+          # 大引けの場合15:15になるので、15:10として扱う
+          datetime -= 60 * 5 if row[4] == "1515"
 
-            data << {
-              datetime: datetime
-            }
+          if row[3] == "003"
+            prev_date = TradingDayJp.prev(datetime.to_date)
+            prev_datetime = Time.new(prev_date.year, prev_date.month, prev_date.day, datetime.hour, datetime.min)
+
+            # ナイトセッションの場合、実際に取引が行われた日時に変換する
+            datetime =
+              if datetime.hour >= 16
+                prev_datetime
+              else
+                prev_datetime + 60 * 60 * 24
+              end
           end
+
+          {
+            datetime: datetime
+          }
         end
 
-        data
+        data.compact
       end
 
       def near_term?(time, contract_month)
--- spec/jpx/price_spec.rb
+++ spec/jpx/price_spec.rb
@@ -2,8 +2,53 @@ require 'spec_helper'
 
 module Jpx
   describe Price do
+    describe "parse" do
+      let(:file) do
+        Tempfile.open("temp") do |f|
+          f.puts "header"
+          f.puts data
+          f.puts
+          f
+        end
+      end
+      let(:price) { Price.parse(file.path)[0] }
+
+      before { file.open }
+
+      context "寄り付き" do
+        let(:data) { "20180104,18,163030018,999,0845,23100,23100,23080,23080,3027,23098.1169,125,10845,201803" }
+
+        it { expect(price[:datetime]).to eq Time.new(2018, 1, 4, 8, 45) }
+      end
+
+      context "大引け" do
+        let(:data) { "20180104,18,163030018,999,1515,23410,23410,23410,23410,3709,23410,3,11515,201803" }
+
+        it { expect(price[:datetime]).to eq Time.new(2018, 1, 4, 15, 10) }
+      end
+
+      context "大納会後の16:30" do
+        let(:data) { "20180104,18,163030018,003,1630,22760,22760,22760,22760,189,22760,5,01630,201803" }
+
+        it { expect(price[:datetime]).to eq Time.new(2017, 12, 29, 16, 30) }
+      end
+
+      context "大納会翌日の05:30" do |variable|
+        let(:data) { "20180104,18,163030018,003,0530,22780,22780,22780,22780,107,22780,1,10530,201803" }
+
+        it { expect(price[:datetime]).to eq Time.new(2017, 12, 30, 5, 30) }
+      end
+
+      context "期先" do
+        let(:data) { "20180104,18,163060018,999,0845,22930,22930,22930,22930,48,22930,2,10845,201806" }
+
+        it { expect(price).to eq nil }
+      end
+    end
+
     describe "self.near_term?" do
       let(:near_term) { Price.near_term?(time, contract_month) }
+
       context "SQ前日の引け" do
         let(:time) { Time.parse "201803081510" }
 
